1262bf66aa2680e6621d9fd2bb8dbb0bd1707e3d
Andrew MacIsaac <amacisaac95@gmail.com>
2023-02-16 09:57:15 -0700

    Lab complete

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedString = "";
+  for (let i = word.length - 1; i >= 0; i--){
+    reversedString += word[i];
+  }
+  console.log(reversedString)
+
+  if (reversedString === word){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+
+  iterate through string to make a new revesed string
+  test if reversed string equals original sting
 */
 
 /*
   Add written explanation of your solution here
+
+  a new reversed string is made by iterating through the given string
+  the new sting is then tested against the original
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +38,7 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  
 }
 
 module.exports = isPalindrome;
